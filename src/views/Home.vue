2b4ba864ea77f3bce299cf685381cc56b081a589
britneykline <87348224+britneykline@users.noreply.github.com>
2021-11-23 20:30:28 -0500

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div id='map' style='width: 400px; height: 300px'></div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
   export default {
     data: function () {
       return {
         message: "Welcome to Vue.js!",
+        map: []
       };
     },
-    created: function () {},
-    methods: {},
+    mounted: function () {
+      this.generateMap();
+    },
+    methods: {
+      generateMap: function() {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_KEY;
+      const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL
+        center: [-77.11, 38.88], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+      });
+    }
+    },
   };
 </script>
